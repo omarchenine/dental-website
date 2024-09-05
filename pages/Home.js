130d3c4d3b7cd33b03a72076ba679672253f564f
omarchenine <147522495+omarchenine@users.noreply.github.com>
2024-09-05 12:25:04 +0100

    Create Home.js

---
+++ pages/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Appointment from '../components/Appointment';
+
+function Home() {
+  return (
+    <div>
+      <h1>Welcome to Our Dental Clinic</h1>
+      <Appointment />
+    </div>
+  );
+}
+
+export default Home;
